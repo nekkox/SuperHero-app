1b1572b2263ce5aab6f1583beb540af5dcc0e5ec
nekkox <klascibox@gmail.com>
2024-02-01 17:52:39 +0100

    Renamed useComics composable into HeroesApi. Refactored HeroesApi and ComicsOverview component

--- src/components/ComicsOverview.vue
+++ src/components/ComicsOverview.vue
@@ -1,38 +1,30 @@
 <script setup>
-import { useComics, useMarvelAPI } from '@/composables/useComics'
+import { useComics } from '@/composables/HeroesApi'
 import { ref, onMounted, watch } from 'vue';
 import LoadingIcon from '@/components/LoadingIcon.vue'
 import ComicCard from '@/components/ComicCard.vue'
 import Pagination from '@/components/Pagination.vue'
-import { useRoute, useRouter } from "vue-router";
+import { useRoute } from "vue-router";
 
 const data = ref()
 const isLoading = ref()
 const currentPage = ref(0)
 const totalPages = ref(0)
 const $route = useRoute();
-const $router = useRouter();
 
 const getComics = async (page = currentPage.value) => {
     isLoading.value = true;
-    //const comics = await useComics(page);
-    //const comics = await useMarvelAPI('comics',{page:page, query:''});
     const comics = await useComics(page)
-    { { console.log(comics) } }
 
     currentPage.value = comics?.offset / comics?.limit || 1;
     totalPages.value = Math.ceil(comics.total / comics.limit);
-    console.log(currentPage.value, totalPages.value);
 
     data.value = comics.results;
     isLoading.value = false;
-    console.log(useComics(page));
 };
 
 onMounted(async () => {
     getComics(+currentPage.value)
-    //useMarvelAPI('comics',{page:+currentPage.value, query:''});
-
 })
 
 if ($route.params.page) {
---
+++ src/composables/HeroesApi.js
@@ -0,0 +1,59 @@
+import { md5 } from 'js-md5'
+
+const publicKey = import.meta.env.VITE_APP_MARVEL_API_PUBLIC
+const privateKey = import.meta.env.VITE_APP_MARVEL_API_PRIVATE
+const MARVEL_API_URL = 'https://gateway.marvel.com/v1/public'
+const ITEMS_PER_PAGE = 20
+const API_SIGN = Api_Sign()
+
+//Construct Authentication credentials
+function Api_Sign() {
+  const ts = Date.now()
+  const params = new URLSearchParams({
+    ts: ts,
+    apikey: publicKey,
+    hash: md5(ts + privateKey + publicKey)
+  })
+  return `${params}`
+}
+
+//Construct request url
+export async function useMarvelAPI(path, options = {}) {
+  const pagination = getPagination(options.page)
+  const query = getQuery(options.query)
+
+  const requestURI = getRequestUri(path, query, pagination)
+
+  return useFetch(requestURI)
+}
+
+export async function useFetch(requestURI) {
+  const res = await fetch(requestURI)
+  if (!res.ok) {
+    throw new Error('Data fetch error')
+  }
+  const jsonRes = await res.json()
+  return jsonRes.data
+}
+
+function getPagination(page) {
+  return page ? `&offset=${page * ITEMS_PER_PAGE}` : ''
+}
+
+function getQuery(query) {
+  return query ? `&${query}` : ''
+}
+
+function getRequestUri(path, pagination, query) {
+  const apiPath = `${MARVEL_API_URL}/${path}`
+  return `${apiPath}?${API_SIGN}${query}${pagination}`
+}
+
+//Main function for fetch comics data
+export const useComics = async (page = 1) => {
+  try {
+    return await useMarvelAPI('comics', { page: page, query: '' })
+  } catch {
+    throw new Error('An error occurred while trying to read comics')
+  }
+}
